6d87a920c11792cd6a0c910057e0a62fee1640b5
wmdaros <wmdaros@bb75d2ad-1e34-79f4-7906-64032645d6eb>
2010-05-16 23:12:47 +0000

    PersonNummer ValueObject

--- patient-context/composites/webcomp/src/main/java/se/vgregion/portal/patient/event/PersonNummer.java
+++ patient-context/composites/webcomp/src/main/java/se/vgregion/portal/patient/event/PersonNummer.java
@@ -19,26 +19,215 @@
 
 package se.vgregion.portal.patient.event;
 
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * This action do that and that, if it has something special it is.
  *
  * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
  */
-public class PersonNummer {
+public class PersonNummer implements Serializable {
     public enum Gender {MALE, FEMALE}
+    public enum Type {SHORT, NORMAL, FULL_NO, FULL, INVALID}
 
-    private String century = "19";
-    private String year = "";
-    private String month = "";
-    private String day = "";
-    private String birthNumber = "";
-    private String checkNumber = "";
-    private String separator = "-"; // or "+" if age over 100
+    private int century;
+    private int year;
+    private int month;
+    private int day;
+    private int birthNumber;
+    private int checkNumber;
+    private String separator; // or "+" if age over 100
 
     private boolean checkNumberValid = false;
     private boolean monthValid = false;
     private boolean dayValid = false;
     private Gender gender;
+    private Type type;
+    private String numberText;
+
+    private PersonNummer(String personnummer) {
+        setNumberText(personnummer);
+    }
+
+    public static PersonNummer personummer(String personnummer) {
+        PersonNummer pNo = new PersonNummer(personnummer);
+        pNo.initYear();
+        pNo.initSeparator();
+        pNo.initCentury();
+        pNo.initMonth();
+        pNo.initDay();
+        pNo.initBirthNumber();
+        pNo.initCheckNumber();
+        pNo.initGender();
+
+        return pNo;
+    }
+
+    public String toShort() {
+        return String.format("%02d%02d%02d%03d%d", year, month, day, birthNumber, checkNumber);
+    }
+
+    public String toNormal() {
+        return String.format("%02d%02d%02d%s%03d%d", year, month, day, separator, birthNumber, checkNumber);
+    }
+
+    public String toFull() {
+        return String.format("%02d%02d%02d%02d-%03d%d", century, year, month, day, birthNumber, checkNumber);
+    }
+
+
+    private void setNumberText(String numberText) {
+        this.numberText = numberText;
+        this.initType();
+    }
+
+    private void initGender() {
+        if (type == Type.INVALID) gender = null;
+        else gender = (birthNumber%2 == 0) ? Gender.FEMALE : Gender.MALE;
+    }
+
+    private void initCheckNumber() {
+        if (type == Type.INVALID) {
+            checkNumber = -1;
+            checkNumberValid = false;
+        } else {
+            checkNumber = Integer.parseInt(numberText.substring(numberText.length()-1));
+            int checkDigit = checkDigitCalculator(toShort());
+            checkNumberValid = checkDigit == checkNumber;
+        }
+    }
+
+    private void initBirthNumber() {
+        if (type == Type.INVALID) birthNumber = -1;
+
+        if (type == Type.SHORT) birthNumber = Integer.parseInt(numberText.substring(6, 9));
+
+        if (type == Type.NORMAL) birthNumber = Integer.parseInt(numberText.substring(7, 10));
+
+        if (type == Type.FULL_NO) birthNumber = Integer.parseInt(numberText.substring(8, 11));
+
+        if (type == Type.FULL) birthNumber = Integer.parseInt(numberText.substring(9, 12));
+    }
+
+    private void initDay() {
+        if (type == Type.INVALID) day = -1;
+        
+        if (type == Type.SHORT || type == Type.NORMAL) day = Integer.parseInt(numberText.substring(4, 6));
+
+        if (type == Type.FULL_NO || type == Type.FULL) day = Integer.parseInt(numberText.substring(6, 8));
+
+        // is day valid
+        String datePart = String.format("%02d%02d%02d%02d", century, year, month, day);
+        DateFormat df = new SimpleDateFormat("yyyyMMdd");
+        try {
+            Date d = df.parse(datePart);
+            String back = df.format(d);
+
+            dayValid = datePart.equals(back);
+        } catch (ParseException e) {
+            // never happends
+            e.printStackTrace();
+        }
+    }
+
+    private void initMonth() {
+        if (type == Type.INVALID) month = -1;
+
+        if (type == Type.SHORT || type == Type.NORMAL) {
+            month = Integer.parseInt(numberText.substring(2, 4));
+        }
+
+        if (type == Type.FULL_NO || type == Type.FULL) {
+            month = Integer.parseInt(numberText.substring(4, 6));
+        }
+
+        monthValid = (month > 0 && month < 13) ? true : false;
+    }
+
+    private void initSeparator() {
+        if (type == Type.INVALID) separator = null;
+
+        if (type == Type.SHORT) separator = "-";
+
+        if (type == Type.NORMAL) separator = numberText.substring(6, 7);
+
+        if (type == Type.FULL_NO || type == Type.FULL) {
+            Calendar cal = Calendar.getInstance();
+            int thisFullYear = cal.get(Calendar.YEAR);
+            int fullYear = Integer.parseInt(numberText.substring(0, 4));
+
+            separator = (thisFullYear - fullYear >= 100) ? "+" : "-";
+        }
+    }
+
+    /**
+     * Use separator and year to determine century - Never use indata directly.
+     */
+    private void initCentury() {
+        if (type == Type.INVALID) century = -1;
+
+        Calendar cal = Calendar.getInstance();
+        int thisCentury = cal.get(Calendar.YEAR)/100;
+        int thisYear = cal.get(Calendar.YEAR)%100;
+
+        if ("-".equals(separator)) {
+            if (year > thisYear) {
+                century = thisCentury -1;
+            } else {
+                century = thisCentury;
+            }
+        }
+
+        if ("+".equals(separator)) {
+            if (year > thisYear) {
+                century = thisCentury -2;
+            } else {
+                century = thisCentury -1;
+            }
+        }
+    }
+
+    private void initYear() {
+        if (type == Type.INVALID) {
+            year = -1;
+        }
+
+        if (type == Type.SHORT || type == Type.NORMAL) {
+            year = Integer.parseInt(numberText.substring(0, 2));
+        }
+
+        if (type == Type.FULL_NO || type == Type.FULL) {
+            year = Integer.parseInt(numberText.substring(2, 4));
+        }
+    }
+
+    private void initType() {
+        Type type;
+
+        switch (numberText.length()) {
+            case 10:
+                type = numberText.matches("\\d{10}") ? Type.SHORT : Type.INVALID;
+                break;
+            case 11:
+                type = numberText.matches("\\d{6}[-|+]\\d{4}") ? Type.NORMAL : Type.INVALID;
+                break;
+            case 12:
+                type = numberText.matches("\\d{12}") ? Type.FULL_NO : Type.INVALID;
+                break;
+            case 13:
+                type = numberText.matches("\\d{8}[-|+]\\d{4}") ? Type.FULL : Type.INVALID;
+                break;
+            default:
+                type = Type.INVALID;
+        }
+
+        this.type = type;
+    }
 
     // valid input:
     // 6509124696
@@ -79,4 +268,51 @@ public class PersonNummer {
         return checkDigit;
     }
 
+    public int getCentury() {
+        return century;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getBirthNumber() {
+        return birthNumber;
+    }
+
+    public int getCheckNumber() {
+        return checkNumber;
+    }
+
+    public String getSeparator() {
+        return separator;
+    }
+
+    public boolean isCheckNumberValid() {
+        return checkNumberValid;
+    }
+
+    public boolean isMonthValid() {
+        return monthValid;
+    }
+
+    public boolean isDayValid() {
+        return dayValid;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Type getType() {
+        return type;
+    }
 }
---
+++ patient-context/composites/webcomp/src/test/java/se/vgregion/portal/patient/event/PersonNummerTest.java
@@ -0,0 +1,333 @@
+/**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+
+package se.vgregion.portal.patient.event;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+
+/**
+ * This action do that and that, if it has something special it is.
+ *
+ * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
+ */
+public class PersonNummerTest {
+    PersonNummer pNo;
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testType() throws Exception {
+        assertEquals("Wrong type", PersonNummer.Type.SHORT, PersonNummer.personummer("6509124696").getType());
+        assertEquals("Wrong type", PersonNummer.Type.NORMAL, PersonNummer.personummer("650912-4696").getType());
+        assertEquals("Wrong type", PersonNummer.Type.NORMAL, PersonNummer.personummer("650912+4696").getType());
+        assertEquals("Wrong type", PersonNummer.Type.FULL_NO, PersonNummer.personummer("196509124696").getType());
+        assertEquals("Wrong type", PersonNummer.Type.FULL, PersonNummer.personummer("19650912-4696").getType());
+        assertEquals("Wrong type", PersonNummer.Type.FULL, PersonNummer.personummer("19650912+4696").getType());
+
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("650912 4696").getType());
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("19650912 4696").getType());
+
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("196509120").getType());
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("19650912+46962").getType());
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("650912469a").getType());
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("650912-469a").getType());
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("65091204696").getType());
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("650912/4696").getType());
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("19650912469a").getType());
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("19650912-469a").getType());
+        assertEquals("Wrong type", PersonNummer.Type.INVALID, PersonNummer.personummer("19650912_4696").getType());
+    }
+
+    @Test
+    public void testSeparator() throws Exception {
+        Calendar cal = Calendar.getInstance();
+        int thisYear = cal.get(Calendar.YEAR)%100;
+
+        pNo = PersonNummer.personummer(String.format("%02d1212121a", thisYear));
+        assertEquals(null, pNo.getSeparator());
+
+        pNo = PersonNummer.personummer(String.format("%02d1212-1212", thisYear));
+        assertEquals("-", pNo.getSeparator());
+
+        pNo = PersonNummer.personummer(String.format("%02d1212+1212", thisYear));
+        assertEquals("+", pNo.getSeparator());
+
+        pNo = PersonNummer.personummer(String.format("%02d12121212", thisYear));
+        assertEquals("-", pNo.getSeparator());
+
+        pNo = PersonNummer.personummer(String.format("19%02d1212-1212", thisYear));
+        assertEquals("+", pNo.getSeparator());
+
+        pNo = PersonNummer.personummer(String.format("19%02d1212-1212", thisYear-1));
+        assertEquals("+", pNo.getSeparator());
+
+        pNo = PersonNummer.personummer(String.format("19%02d1212-1212", thisYear+1));
+        assertEquals("-", pNo.getSeparator());
+
+        pNo = PersonNummer.personummer(String.format("19%02d1212+1212", thisYear+1));
+        assertEquals("-", pNo.getSeparator());
+
+        pNo = PersonNummer.personummer(String.format("19%02d1212+1212", thisYear-1));
+        assertEquals("+", pNo.getSeparator());
+    }
+
+    @Test
+    public void testCentury() throws Exception {
+        pNo = PersonNummer.personummer("6509124696");
+        assertEquals(19, pNo.getCentury());
+
+        pNo = PersonNummer.personummer("650912-4696");
+        assertEquals(19, pNo.getCentury());
+
+        pNo = PersonNummer.personummer("650912+4696");
+        assertEquals(18, pNo.getCentury());
+
+        pNo = PersonNummer.personummer("990912-4696");
+        assertEquals(19, pNo.getCentury());
+
+        pNo = PersonNummer.personummer("990912+4696");
+        assertEquals(18, pNo.getCentury());
+
+        pNo = PersonNummer.personummer("000912-4696");
+        assertEquals(20, pNo.getCentury());
+
+        pNo = PersonNummer.personummer("000912+4696");
+        assertEquals(19, pNo.getCentury());
+
+        Calendar cal = Calendar.getInstance();
+        int thisYear = cal.get(Calendar.YEAR)%100;
+
+        pNo = PersonNummer.personummer(String.format("%02d1212-1212", thisYear));
+        assertEquals(20, pNo.getCentury());
+
+        pNo = PersonNummer.personummer(String.format("%02d1212+1212", thisYear));
+        assertEquals(19, pNo.getCentury());
+
+        pNo = PersonNummer.personummer(String.format("%02d1212-1212", thisYear+1));
+        assertEquals(19, pNo.getCentury());
+
+        pNo = PersonNummer.personummer(String.format("%02d12121212", thisYear+1));
+        assertEquals(19, pNo.getCentury());
+
+        pNo = PersonNummer.personummer(String.format("%02d12121212", thisYear));
+        assertEquals(20, pNo.getCentury());
+    }
+
+    @Test
+    public void testYear() throws Exception {
+        assertEquals("Wrong year", 65, PersonNummer.personummer("6509124696").getYear());
+        assertEquals("Wrong year", 65, PersonNummer.personummer("650912-4696").getYear());
+        assertEquals("Wrong year", 65, PersonNummer.personummer("650912+4696").getYear());
+        assertEquals("Wrong year", 65, PersonNummer.personummer("196509124696").getYear());
+        assertEquals("Wrong year", 65, PersonNummer.personummer("19650912-4696").getYear());
+        assertEquals("Wrong year", 65, PersonNummer.personummer("19650912+4696").getYear());
+        assertEquals("Wrong year", -1, PersonNummer.personummer("650912 4696").getYear());
+    }
+
+    @Test
+    public void testMonth() throws Exception {
+        pNo = PersonNummer.personummer("6509124696");
+        assertEquals("Wrong month", 9, pNo.getMonth());
+        assertTrue(pNo.isMonthValid());
+
+        pNo = PersonNummer.personummer("650912-4696");
+        assertEquals("Wrong month", 9, pNo.getMonth());
+        assertTrue(pNo.isMonthValid());
+
+        pNo = PersonNummer.personummer("650912+4696");
+        assertEquals("Wrong month", 9, pNo.getMonth());
+        assertTrue(pNo.isMonthValid());
+
+        pNo = PersonNummer.personummer("196509124696");
+        assertEquals("Wrong month", 9, pNo.getMonth());
+        assertTrue(pNo.isMonthValid());
+
+        pNo = PersonNummer.personummer("19650912-4696");
+        assertEquals("Wrong month", 9, pNo.getMonth());
+        assertTrue(pNo.isMonthValid());
+
+        pNo = PersonNummer.personummer("19650912+4696");
+        assertEquals("Wrong month", 9, pNo.getMonth());
+        assertTrue(pNo.isMonthValid());
+
+        pNo = PersonNummer.personummer("650912 4696");
+        assertEquals("Wrong month", -1, pNo.getMonth());
+        assertFalse(pNo.isMonthValid());
+
+        pNo = PersonNummer.personummer("19652312+4696");
+        assertEquals("Wrong month", 23, pNo.getMonth());
+        assertFalse(pNo.isMonthValid());
+                
+        pNo = PersonNummer.personummer("19652312+4696");
+        assertEquals("Wrong month", 23, pNo.getMonth());
+        assertFalse(pNo.isMonthValid());
+
+        pNo = PersonNummer.personummer("19652312+4696");
+        assertEquals("Wrong month", 23, pNo.getMonth());
+        assertFalse(pNo.isMonthValid());
+    }
+
+    @Test
+    public void testDay() throws Exception {
+        pNo = PersonNummer.personummer("191212121212");
+        assertEquals(12, pNo.getDay());
+        assertTrue(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("19121212121a");
+        assertEquals(-1, pNo.getDay());
+        assertFalse(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("1212121212");
+        assertEquals(12, pNo.getDay());
+        assertTrue(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("121212-1212");
+        assertEquals(12, pNo.getDay());
+        assertTrue(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("1202301212");
+        assertEquals(30, pNo.getDay());
+        assertFalse(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("1212311212");
+        assertEquals(31, pNo.getDay());
+        assertTrue(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("1212321212");
+        assertEquals(32, pNo.getDay());
+        assertFalse(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("20000228-1212");
+        assertEquals(28, pNo.getDay());
+        assertTrue(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("20000229-1212");
+        assertEquals(29, pNo.getDay());
+        assertTrue(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("20000230-1212");
+        assertEquals(30, pNo.getDay());
+        assertFalse(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("20090228-1212");
+        assertEquals(28, pNo.getDay());
+        assertTrue(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("20090229-1212");
+        assertEquals(29, pNo.getDay());
+        assertFalse(pNo.isDayValid());
+
+        pNo = PersonNummer.personummer("20090230-1212");
+        assertEquals(30, pNo.getDay());
+        assertFalse(pNo.isDayValid());
+    }
+
+    @Test
+    public void testBirthNumber() {
+        assertEquals(121, PersonNummer.personummer("1212121212").getBirthNumber());
+        assertEquals(121, PersonNummer.personummer("121212-1212").getBirthNumber());
+        assertEquals(121, PersonNummer.personummer("19121212-1212").getBirthNumber());
+        assertEquals(121, PersonNummer.personummer("191212121212").getBirthNumber());
+        assertEquals(121, PersonNummer.personummer("191212121212").getBirthNumber());
+        assertEquals(-1, PersonNummer.personummer("a91212121212").getBirthNumber());
+    }
+
+
+    @Test
+    public void testCheckNumber() {
+        assertEquals(2, PersonNummer.personummer("1212121212").getCheckNumber());
+        assertEquals(2, PersonNummer.personummer("121212-1212").getCheckNumber());
+        assertEquals(2, PersonNummer.personummer("19121212-1212").getCheckNumber());
+        assertEquals(2, PersonNummer.personummer("191212121212").getCheckNumber());
+        assertEquals(2, PersonNummer.personummer("191212121212").getCheckNumber());
+        assertEquals(-1, PersonNummer.personummer("a91212121212").getCheckNumber());
+    }
+
+    @Test
+    public void testCheckNumberValid() {
+        pNo = PersonNummer.personummer("1212121212");
+        assertTrue(pNo.isCheckNumberValid());
+
+        pNo = PersonNummer.personummer("121212-1212");
+        assertTrue(pNo.isCheckNumberValid());
+
+        pNo = PersonNummer.personummer("191212121212");
+        assertTrue(pNo.isCheckNumberValid());
+
+        pNo = PersonNummer.personummer("19121212-1212");
+        assertTrue(pNo.isCheckNumberValid());
+
+        pNo = PersonNummer.personummer("19121212-1211");
+        assertFalse(pNo.isCheckNumberValid());
+
+        pNo = PersonNummer.personummer("19221212-1212");
+        assertFalse(pNo.isCheckNumberValid());
+
+        pNo = PersonNummer.personummer("18121212-1212");
+        assertTrue(pNo.isCheckNumberValid());
+    }
+
+    @Test
+    public void testGender() {
+        assertEquals(PersonNummer.Gender.MALE, PersonNummer.personummer("1212121212").getGender());
+        assertEquals(PersonNummer.Gender.FEMALE, PersonNummer.personummer("121212-1222").getGender());
+        assertNull(PersonNummer.personummer("a21212-1222").getGender());
+    }
+
+    @Test
+    public void testFormat() {
+        pNo = PersonNummer.personummer("1212121212");
+        assertEquals("1212121212", pNo.toShort());
+        assertEquals("121212-1212", pNo.toNormal());
+        assertEquals("19121212-1212", pNo.toFull());
+
+        pNo = PersonNummer.personummer("121212-1212");
+        assertEquals("1212121212", pNo.toShort());
+        assertEquals("121212-1212", pNo.toNormal());
+        assertEquals("19121212-1212", pNo.toFull());
+
+        pNo = PersonNummer.personummer("121212+1212");
+        assertEquals("1212121212", pNo.toShort());
+        assertEquals("121212+1212", pNo.toNormal());
+        assertEquals("18121212-1212", pNo.toFull());
+
+        pNo = PersonNummer.personummer("19121212-1212");
+        assertEquals("1212121212", pNo.toShort());
+        assertEquals("121212-1212", pNo.toNormal());
+        assertEquals("19121212-1212", pNo.toFull());
+
+        pNo = PersonNummer.personummer("18121212-1212");
+        assertEquals("1212121212", pNo.toShort());
+        assertEquals("121212+1212", pNo.toNormal());
+        assertEquals("18121212-1212", pNo.toFull());
+
+        pNo = PersonNummer.personummer("18121212+1212");
+        assertEquals("1212121212", pNo.toShort());
+        assertEquals("121212+1212", pNo.toNormal());
+        assertEquals("18121212-1212", pNo.toFull());
+    }
+}
