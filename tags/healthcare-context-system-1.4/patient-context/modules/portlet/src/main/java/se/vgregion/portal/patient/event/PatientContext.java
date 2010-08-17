28efc02ae005e7fc834b48e585d789c4557f3b8d
asse.hemma <asse.hemma@bb75d2ad-1e34-79f4-7906-64032645d6eb>
2010-08-17 12:48:14 +0000

    [maven-release-plugin]  copy for tag healthcare-context-system-1.4

---
+++ tags/healthcare-context-system-1.4/patient-context/modules/portlet/src/main/java/se/vgregion/portal/patient/event/PatientContext.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+
+package se.vgregion.portal.patient.event;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This action do that and that, if it has something special it is.
+ *
+ * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
+ */
+public class PatientContext implements Serializable {
+    private static final long serialVersionUID = 5623507583753192525L;
+
+    private PatientEvent currentPatient;
+
+    private List<PatientEvent> patientHistory = new ArrayList<PatientEvent>();
+
+    /**
+     * Access the currently active PatientEvent.
+     *
+     * @return PatientEvent
+     */
+    public PatientEvent getCurrentPatient() {
+        return currentPatient;
+    }
+
+    /**
+     * Set the currently active PatientEvent
+     *
+     * @param currentPatient PatientEvent
+     */
+    public void setCurrentPatient(PatientEvent currentPatient) {
+        this.currentPatient = currentPatient;
+    }
+
+    /**
+     * Access the context history.
+     *
+     * @return List
+     */
+    public List<PatientEvent> getPatientHistory() {
+        return Collections.unmodifiableList(patientHistory);
+    }
+
+    /**
+     * Add a PatientEvent object to the context history.
+     * Duplications are not saved.
+     *
+     * @param patient PatientEvent.
+     */
+    public void addToHistory(PatientEvent patient) {
+        if (!patientHistory.contains(patient)) {
+            patientHistory.add(patient);
+        }
+    }
+
+    /**
+     * Utility method to access context history size.
+     *
+     * @return Integer
+     */
+    public int getPatientHistorySize() {
+        return patientHistory.size();
+    }
+
+    public void clear() {
+        currentPatient = null;
+    }
+}
---
+++ tags/healthcare-context-system-1.4/patient-context/modules/portlet/src/main/webapp/WEB-INF/jsp/patient/configuration.jsp
@@ -0,0 +1,36 @@
+<%--
+
+    Copyright 2010 Västra Götalandsregionen
+
+      This library is free software; you can redistribute it and/or modify
+      it under the terms of version 2.1 of the GNU Lesser General Public
+      License as published by the Free Software Foundation.
+
+      This library is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU Lesser General Public License for more details.
+
+      You should have received a copy of the GNU Lesser General Public
+      License along with this library; if not, write to the
+      Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+      Boston, MA 02111-1307  USA
+
+
+--%>
+
+<%--
+  Created by IntelliJ IDEA.
+  User: david
+  Date: May 10, 2010
+  Time: 12:38:44 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head><title>Konfiguration</title></head>
+  <body>
+    <h1>Konfiguration</h1>
+    Det finns ännu inga konfigurations möjligheter för denna portlet.
+  </body>
+</html>
